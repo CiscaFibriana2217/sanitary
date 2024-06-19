1d9d6d9776bbe5a40c3dc306e963830250b84568
CiscaFibriana2217 <cisca.fibriana2217@gmail.com>
2024-06-19 17:41:09 +0700

    Update sanitary_model.py

--- sanitary_model.py
+++ sanitary_model.py
@@ -22,13 +22,16 @@ with col1 :
 with col2 :
     Display_Value = st.text_input ('Input Nilai Display Value')
 
+with col1 :
+    Numeric = st.text_input ('Input Nilai Numeric')
+
 
 # code untuk prediksi
 sanitary_level = ''
 
 # membuat tombol untuk prediksi
 if st.button('Lihat Akurasi'):
-    sanitary_prediction = sanitary_model.predict([[Year,'WHO region','Country','Residence Area Type','Display Value']])
+    sanitary_prediction = sanitary_model.predict([[Year,'WHO region','Country','Residence Area Type','Display Value','Numeric]])
 
     if(sanitary_prediction[0] == 0):
         sanitary_predict = 'Tingkat Sanitasi Rendah'
