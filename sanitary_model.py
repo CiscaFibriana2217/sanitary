3f452d03d45f389bb40dffa849c4bb4483acf23f
CiscaFibriana2217 <cisca.fibriana2217@gmail.com>
2024-06-20 22:18:19 +0700

    Update sanitary_model.py

--- sanitary_model.py
+++ sanitary_model.py
@@ -1,46 +1,32 @@
 import pickle
 import streamlit as st
 
-# Membaca model
-sanitary_model = pickle.load(open('sanitary_model.sav','rb'))
+# Fungsi utama untuk aplikasi Streamlit
+def main():
+    st.title("Prediksi Tingkat Sanitasi")
 
-# Judul web
-st.title('Tingkat Sanitasi pada Negara di Dunia')
+    # Memuat data dan melatih model
+    data = load_data()
+    model, scaler, label_encoders = train_model(data)
 
-# Membagi kolom
-col1, col2 = st.columns(2)
+    # Input pengguna
+    st.header("Input Data")
+    year = st.number_input("Tahun", min_value=2000, max_value=2023, value=2020)
+    country = st.selectbox("Negara", data['Country'].unique())
+    residence_area = st.selectbox("Area Tempat Tinggal", data['Residence Area Type'].unique())
+    display_value = st.number_input("Nilai Display", min_value=0.0, max_value=100.0, value=50.0)
 
-with col1:
-    Year = st.text_input('Input Nilai Tahun')
-
-with col2:
-    Country = st.text_input('Input Negara')
-
-with col1:
-    Residence_Area_Type = st.text_input('Input Residence Area Type')
-
-with col2:
-    Display_Value = st.text_input('Input Nilai Display Value')
-
-with col1:
-    WHO_region = st.text_input('Input WHO Region')
-
-# Inisialisasi variabel untuk hasil prediksi
-sanitary_level = ''
-
-# Membuat tombol untuk prediksi
-if st.button('Lihat Akurasi'):
-    if Year and WHO_region and Country and Residence_Area_Type and Display_Value:
-        sanitary_prediction = sanitary_model.predict([[Year, WHO_region, Country, Residence_Area_Type, Display_Value]])
-
-        if sanitary_prediction[0] == 0:
-            sanitary_predict = 'Tingkat Sanitasi Rendah'
+    # Melakukan prediksi saat tombol diklik
+    if st.button("Prediksi"):
+        input_data = (year, country, residence_area, display_value)
+        result = predict(model, scaler, label_encoders, input_data)
+        
+        if result < 0.5:
+            st.write('Tingkat Sanitasi Rendah')
         else:
-            sanitary_predict = 'Tingkat Sanitasi Tinggi'
+            st.write('Tingkat Sanitasi Tinggi')
 
-        st.success(sanitary_predict)
-    else:
-        st.error("Harap isi semua kolom input.")
+# Menjalankan aplikasi Streamlit
+if __name__ == "__main__":
+    main()
 
-    st.success()
-    
