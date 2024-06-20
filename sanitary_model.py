065f3708df0c987d02ff7ba097afaf628339fe07
CiscaFibriana2217 <cisca.fibriana2217@gmail.com>
2024-06-20 15:10:23 +0700

    Update sanitary_model.py

--- sanitary_model.py
+++ sanitary_model.py
@@ -1,42 +1,46 @@
 import pickle
 import streamlit as st
 
-# membaca model
+# Membaca model
 sanitary_model = pickle.load(open('sanitary_model.sav','rb'))
 
-# judul web
+# Judul web
 st.title('Tingkat Sanitasi pada Negara di Dunia')
 
-# membagi kolom
+# Membagi kolom
 col1, col2 = st.columns(2)
 
-with col1 :
-    Year = st.text_input ('Input Tahun')
+with col1:
+    Year = st.text_input('Input Nilai Tahun')
 
-with col2 :
-    Country = st.text_input ('Input Negara')
+with col2:
+    Country = st.text_input('Input Negara')
 
-with col1 :
-    Residence_Area_Type = st.text_input ('Input Residence Area Type')
+with col1:
+    Residence_Area_Type = st.text_input('Input Residence Area Type')
 
-with col2 :
-    Display_Value = st.text_input ('Input Nilai Display Value')
+with col2:
+    Display_Value = st.text_input('Input Nilai Display Value')
 
-with col1 :
-   WHO_region = st.text_input ('Input Wilayah WHO')
+with col1:
+    WHO_region = st.text_input('Input WHO Region')
 
-
-# code untuk prediksi
+# Inisialisasi variabel untuk hasil prediksi
 sanitary_level = ''
 
-# membuat tombol untuk prediksi
+# Membuat tombol untuk prediksi
 if st.button('Lihat Akurasi'):
-    sanitary_prediction = sanitary_model.predict([[Year,'WHO region','Country','Residence Area Type','Display Value']])
+    if Year and WHO_region and Country and Residence_Area_Type and Display_Value:
+        sanitary_prediction = sanitary_model.predict([[Year, WHO_region, Country, Residence_Area_Type, Display_Value]])
 
-    if(sanitary_prediction[0] == 0):
-        sanitary_predict = 'Tingkat Sanitasi Rendah'
-    else :
-        sanitary_predict = 'Tingkat Sanitasi Tinggi'
+        if sanitary_prediction[0] == 0:
+            sanitary_predict = 'Tingkat Sanitasi Rendah'
+        else:
+            sanitary_predict = 'Tingkat Sanitasi Tinggi'
+
+        st.success(sanitary_predict)
+    else:
+        st.error("Harap isi semua kolom input.")
 
     st.success()
     
