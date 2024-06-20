f06427313783dc4e7baae6e463be22e5ab3f05dc
CiscaFibriana2217 <cisca.fibriana2217@gmail.com>
2024-06-20 22:17:48 +0700

    Update Sanitasi revise.py

--- Sanitasi revise.py
+++ Sanitasi revise.py
@@ -55,31 +55,3 @@ def predict(model, scaler, label_encoders, input_data):
     prediction = model.predict(input_data_scaled)
     return prediction[0]
 
-# Fungsi utama untuk aplikasi Streamlit
-def main():
-    st.title("Prediksi Tingkat Sanitasi")
-
-    # Memuat data dan melatih model
-    data = load_data()
-    model, scaler, label_encoders = train_model(data)
-
-    # Input pengguna
-    st.header("Input Data")
-    year = st.number_input("Tahun", min_value=2000, max_value=2023, value=2020)
-    country = st.selectbox("Negara", data['Country'].unique())
-    residence_area = st.selectbox("Area Tempat Tinggal", data['Residence Area Type'].unique())
-    display_value = st.number_input("Nilai Display", min_value=0.0, max_value=100.0, value=50.0)
-
-    # Melakukan prediksi saat tombol diklik
-    if st.button("Prediksi"):
-        input_data = (year, country, residence_area, display_value)
-        result = predict(model, scaler, label_encoders, input_data)
-        
-        if result < 0.5:
-            st.write('Tingkat Sanitasi Rendah')
-        else:
-            st.write('Tingkat Sanitasi Tinggi')
-
-# Menjalankan aplikasi Streamlit
-if __name__ == "__main__":
-    main()
